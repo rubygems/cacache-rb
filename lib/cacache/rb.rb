1832c6eccea5a43795f976ca874ef0967049d328
Samuel Giddins <segiddins@segiddins.me>
2017-08-02 18:00:57 -0500

    Initial Commit

---
+++ lib/cacache/rb.rb
@@ -0,0 +1,7 @@
+require "cacache/rb/version"
+
+module Cacache
+  module Rb
+    # Your code goes here...
+  end
+end
---
+++ spec/cacache/rb_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Cacache::Rb do
+  it "has a version number" do
+    expect(Cacache::Rb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
