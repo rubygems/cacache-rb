02e11be62d3365d2793b5e307227bb475532434f
Samuel Giddins <segiddins@segiddins.me>
2017-08-04 18:56:29 -0500

    Avoid the need to use #send in the specs

--- spec/cacache/cache_spec.rb
+++ spec/cacache/cache_spec.rb
@@ -11,11 +11,21 @@ RSpec.describe CACache::Cache do
 
   after { FileUtils.rm_rf cache_path }
 
-  subject(:cache) { described_class.new(cache_path) }
+  subject(:cache) do
+    described_class.new(cache_path).tap do |cache|
+      cache.singleton_class.send(:public, *cache.private_methods(false))
+    end
+  end
+
+  let(:empty_cache) do
+    described_class.new("").tap do |cache|
+      cache.singleton_class.send(:public, *cache.private_methods(false))
+    end
+  end
 
   def cache_content(entries = {})
     entries.reduce({}) do |acc, (k, content)|
-      cpath = described_class.new("").send(:content_path, k)
+      cpath = empty_cache.content_path(k)
       dir = acc
       cpath.dirname.descend {|v| dir = (dir[v.basename.to_s] ||= {}) }
       dir[cpath.basename.to_s] = content
@@ -24,9 +34,8 @@ RSpec.describe CACache::Cache do
   end
 
   def cache_index(entries = {})
-    cache = described_class.new("")
     entries.reduce({}) do |acc, (k, content)|
-      cpath = cache.send(:bucket_path, k)
+      cpath = empty_cache.bucket_path(k)
       dir = acc
       cpath.dirname.descend {|v| dir = (dir[v.basename.to_s] ||= {}) }
       dir[cpath.basename.to_s] =
@@ -37,8 +46,8 @@ RSpec.describe CACache::Cache do
           content = [content] if content.is_a?(Hash)
           content.map do |e|
             json = e.to_json
-            e[:path] ||= cache.send(:content_path, e[:integrity])
-            "#{cache.send(:hash_entry, json)}\t#{json}\n"
+            e[:path] ||= cache.content_path(e[:integrity])
+            "#{cache.hash_entry(json)}\t#{json}\n"
           end.join
         end
       acc
@@ -50,7 +59,7 @@ RSpec.describe CACache::Cache do
       content = "foobarbaz"
       integrity = ssri.from_data(content)
       fixture_tree.merge(cache_content(integrity => content))
-      data = cache.send(:read, integrity, {})
+      data = cache.read(integrity, {})
       expect(data).to eq content
     end
   end
@@ -59,13 +68,13 @@ RSpec.describe CACache::Cache do
     it "returns { sri, size } when a cache file exists" do
       fixture_tree.merge(cache_content("sha1-deadbeef" => ""))
 
-      content = cache.send(:has_content, "sha1-deadbeef")
+      content = cache.has_content("sha1-deadbeef")
       expect(content).to match(
         :sri => have_attributes(:to_s => "sha1-deadbeef"),
         :size => 0
       )
 
-      content = cache.send(:has_content, "sha1-deadc000")
+      content = cache.has_content("sha1-deadc000")
       expect(content).to be false
     end
   end
@@ -79,7 +88,7 @@ RSpec.describe CACache::Cache do
   describe "#get" do
     it "gets data in bulk" do
       fixture_tree.merge(cache_content(integrity => content))
-      cache.send(:index_insert, key, integrity, opts)
+      cache.index_insert(key, integrity, opts)
 
       res = cache.get(key)
       expect(res).to eq(:data => content,
@@ -100,7 +109,7 @@ RSpec.describe CACache::Cache do
   describe "#get_info" do
     it "returns the index entry" do
       integrity = ssri.from_data(content)
-      inserted_entry = cache.send(:index_insert, key, integrity, opts)
+      inserted_entry = cache.index_insert(key, integrity, opts)
 
       entry = cache.get_info(key)
       expect(entry.to_h).to eq inserted_entry.to_h
@@ -112,7 +121,7 @@ RSpec.describe CACache::Cache do
       inserted_integrity = cache.put(key, content)
       expect(inserted_integrity).to eq integrity
 
-      data_path = cache.send(:content_path, integrity)
+      data_path = cache.content_path(integrity)
       data = File.read(data_path)
       expect(data).to eq content
     end
@@ -127,7 +136,7 @@ RSpec.describe CACache::Cache do
     describe "#rm_all" do
       it "deletes all content and index dirs" do
         fixture_tree.merge(cache_content(integrity => content))
-        cache.send(:index_insert, key, integrity, :metadata => metadata)
+        cache.index_insert(key, integrity, :metadata => metadata)
         cache_path.join("tmp").mkdir
         cache_path.join("other.rb").open("w") {|f| f << "hi" }
 
@@ -158,9 +167,9 @@ RSpec.describe CACache::Cache do
       }
       fixture_tree.merge cache_index(contents)
 
-      expect(cache.ls).to eq Hash[contents.map {|k, e| [k, cache.send(:format_entry, e)] }]
+      expect(cache.ls).to eq Hash[contents.map {|k, e| [k, cache.format_entry(e)] }]
 
-      expect {|b| cache.ls(&b) }.to yield_successive_args(*contents.values.map {|e| cache.send(:format_entry, e) })
+      expect {|b| cache.ls(&b) }.to yield_successive_args(*contents.values.map {|e| cache.format_entry(e) })
     end
 
     it "handles separate keys in conflicting buckets" do
@@ -182,7 +191,7 @@ RSpec.describe CACache::Cache do
       }
       fixture_tree.merge cache_index("whatever" => contents.values)
 
-      expect(cache.ls).to eq Hash[contents.map {|k, e| [k, cache.send(:format_entry, e)] }]
+      expect(cache.ls).to eq Hash[contents.map {|k, e| [k, cache.format_entry(e)] }]
     end
 
     it "works on an empty cache" do
